1f47439e01d6127836f6b66c64582274130af60e
shahanSD <abc@gmail.com>
2026-09-15 06:51:10 +0600

    Prepare Django settings for deployment

--- office/settings.py
+++ office/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mi5!t$*m&qo5xb4a30d3xr$_k0at_0kohma#klvr$g_bb+wi-m'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-development-key"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = os.environ.get("DEBUG", "True") == "True"
+#
 
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
